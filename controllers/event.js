c932368e0ec16873dc837b84b449a0c3561c2ab5
Pc_Huy <pchuy4003@gmai.com>
2024-04-04 20:14:01 +0700

    Replace error to respone

--- controllers/event.js
+++ controllers/event.js
@@ -1,23 +1,45 @@
 // Import module for event controller
 const Event = require('../models/Event')
 
+// Respone function
+const sendRespone = (res, data, message, status = 201, pagination = {}) =>{
+  return res.status(status).json({
+    data: [ data ],
+    pagination,
+    message
+  })
+}
+
+// Sort events by date time
+const sortEventsByDateTime = (events) => {
+  events.sort((a, b) => {
+    const dateA = new Date(`${a.date} ${a.time}`)
+    const dateB = new Date(`${b.date} ${b.time}`)
+    return dateA - dateB
+  })
+  events.reverse();
+  return events
+}
+
+// Sort events by views
+const sortEventsByViews = (events) => {
+    events.sort((eventA, eventB) => eventB.views - eventA.views);
+    return events;
+};
+
 // Controller for event
 const createNewEvent = async (req, res, next) => {   // Create new Event
   const newEvent = new Event(req.body)
 
   await newEvent.save()
 
-  return res.status(201).json({ 
-    data: [
-      { data: newEvent }
-    ], 
-    paganition: {},
-    message: "Tạo sự kiện mới thành công" 
-  })
+  return sendRespone(res, { data: newEvent }, "Tạo sự kiện mới thành công") 
 }
 
 const getEvents = async (req, res, next) => {      // Get list event
-  let { page, limit, status, eventId, name, host, member, type, startDate, endDate } = req.query
+  let { page, limit, status, eventId, name, host, 
+    member, type, startDate, endDate, sort } = req.query
+
   limit = parseInt(limit) || 8
   page = parseInt(page) || 1
 
@@ -37,32 +59,28 @@ const getEvents = async (req, res, next) => {      // Get list event
       eventQuery.date = { $gte: startDate, $lte: endDate };
     }
 
-    const events = await Event.find(eventQuery).skip(skip).limit(limit)
+    let events = await Event.find(eventQuery).skip(skip).limit(limit)
 
-    events.sort((eventA, eventB) => {
-      const dateTimeA = new Date(`${eventA.date} ${eventA.time}`);
-      const dateTimeB = new Date(`${eventB.date} ${eventB.time}`);
-      return dateTimeA - dateTimeB;
-    });
+    if (events.length === 0) return sendRespone(res, { data: [] }, "Không thể tìm thấy sự kiện!")
 
-    events.reverse();
-
+    if (sort === 'view') { 
+      events = sortEventsByViews(events)
+    } else {
+      events = sortEventsByDateTime(events)
+    }
 
     const totalEvents = await Event.countDocuments(eventQuery)
 
     const totalPages = Math.ceil(totalEvents / limit)
 
-    return res.status(201).json({ 
-      data: [
-        { data: events }
-      ], 
-      pagination: {
-        totalItems: totalEvents,
-        currentPage: page,
-        totalPages: totalPages
-      },
-      message: `${totalEvents} sự kiện đã được tìm thấy!` 
-    })
+    const pagination = {
+      totalItems: totalEvents,
+      currentPage: page,
+      totalPages: totalPages
+    }
+
+    return sendRespone(res, { data: events }, `${totalEvents} sự kiện đã được tìm thấy!`,
+    201, pagination)
   } catch (error) {
     next(error)
   }
@@ -82,23 +100,13 @@ const updateEventById = async (req, res, next) => {   // Update event by id (pat
   try {
     const foundEvent = await Event.findById(eventID)
 
-    if (!foundEvent) {
-      const error = new Error("Không thể tìm thấy sự kiện!")
-      error.status = 404
-      throw error 
-    }
+    if (!foundEvent) return sendRespone(res, { data: [] }, "Không thể tìm thấy sự kiện!")
 
     const newEvent = req.value.body
 
     const updateEvent = await Event.findByIdAndUpdate(eventID, newEvent)
 
-    return res.status(201).json({ 
-      data: [
-        { data: newEvent }
-      ], 
-      paganition: {},
-      message: "Cập nhật thông tin sự kiện thành công!" 
-    })
+    return sendRespone(res, { data: newEvent }, "Cập nhật thông tin sự kiện thành công!") 
   } catch (error) {
     next(error)
   }
