aac86ebcfdc06da22b855fe0341ec6828ac49b23
tranquockhang1810 <tranquockhang1810@gmail.com>
2024-05-04 14:19:02 +0700

    test git

--- utils/calculate.js
+++ utils/calculate.js
@@ -1,7 +1,7 @@
 // Import module for bill controller
 const Bill = require('../models/Bill')
 const Ticket = require('../models/Ticket')
-
+// cc
 const calculateTotalAmountForEvent = async (eventId) => {
   let totalAmount = 0
   const tickets = await Ticket.find({ eventId })
